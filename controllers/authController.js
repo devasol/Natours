537946e3f2724832e12cf080d89d66bf6fe28807
devasol <dawit8908@gmail.com>
2025-10-23 15:24:46 +0300

    Protecting Tour Route step-4 have finished

--- controllers/authController.js
+++ controllers/authController.js
@@ -83,8 +83,8 @@ exports.protect = catchAsync(async (req, res, next) => {
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) check if user still exists
-  const freshUser = await User.findById(decoded.id);
-  if (!freshUser) {
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
     return next(
       new AppError(
         'The user belonging to this token does no longer exist.',
@@ -93,5 +93,13 @@ exports.protect = catchAsync(async (req, res, next) => {
     );
   }
   // 4)Check if the user changed password after the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently chaged password! Please log in again.', 401),
+    );
+  }
+
+  // GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
   next();
 });
